4d575e42f28c5971aa2eff70ce5b6eff8d88c1b9
Sharon Grozman <sharong1337@gmail.com>
2021-03-27 22:24:23 +0300

    add tests

---
+++ tests/specs/tests.spec.ts
@@ -0,0 +1,24 @@
+import {replaceUrlValues} from '../../src';
+
+const url =
+    'https://my-api.mydomain.com/v2/room/59e107ac-f52c-4729-bbce-b666cf7cee2d/messages/59e107ac-f52c-4729-bbce-b666cf7cee2d';
+const expectedUrl = '/v2/room/#val/messages/#val';
+
+const emptyValueUrl =
+    'https://my-api.mydomain.com/v2/room/59e107ac-f52c-4729-bbce-b666cf7cee2d/messages/59e107ac-f52c-4729-bbce-b666cf7cee2d//';
+
+describe('tests', () => {
+    describe('replaceUrlValue', () => {
+        it('should replace 2 guids in #val', async () => {
+            const replaced = replaceUrlValues(url);
+
+            expect(replaced).toBe(expectedUrl);
+        });
+
+        it('should replace 2 guids in #val', async () => {
+            const replaced = replaceUrlValues(emptyValueUrl);
+
+            expect(replaced).toBe(expectedUrl);
+        });
+    });
+});
